84f80e970db3bb2bbe806bf0d04200455f18b002
BIT Student <classroom@bgit.rs>
2019-11-26 09:21:14 +0100

    Package Managers

--- Package _managers/Task_2/client/main.js
+++ Package _managers/Task_2/client/main.js
@@ -2,11 +2,18 @@ fetch('http://127.0.0.1:3000/')
     .then(res => res.json())
     .then((posts) => {
         posts.forEach(post => {
-            var $div = $('<div></div>').append('body')
-            h1.textContent = post.title;
-            $('p').append('div');
-            .text()
+            const h1 = $('<h2></h2>');
+            const div = $('<div></div>');
+            h1.text(post.title);
+            const p = $('<p></p>');
+            p.text(post.body);
 
+            div
+                .append(h1)
+                .append(p)
+                .css('background-color', 'grey')
+                .css('margin', '10px')
+            $('header').append(div)
             // var $p = document.createElement('p');
             // $p.textContent = post.title;
             // document.querySelector('.section').append($p)
